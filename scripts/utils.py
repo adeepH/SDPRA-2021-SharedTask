efacd7fbbf4e28ff115ac25a0571239d371b82c4
Adeep Hande <46108405+adeepH@users.noreply.github.com>
2021-01-01 21:28:36 +0530

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,6 @@
 import torch
 from transformers import *
+from dataset import *
 import pandas as pd
 def create_data_loader(df,tokenizer,max_len,batch_size):
   ds = RFDataset(
@@ -41,7 +42,41 @@ def get_predictions(model, data_loader):
   real_values = torch.stack(real_values).cpu()
   return sentence, predictions, prediction_probs, real_values
   
+import pandas as pd
+from torch.utils.data import Dataset,DataLoader
 
+class RFDataset(Dataset):
+  def __init__(self,text,label,tokenizer,max_len):
+    self.text = text
+    self.label = label
+    self.tokenizer = tokenizer
+    self.max_len = max_len
+  
+  def __len__(self):
+    return len(self.text)
+  
+  def __getitem__(self,item):
+    text = str(self.text[item])
+    label = self.label[item]
+
+    encoding = self.tokenizer.encode_plus(
+        text,
+        add_special_tokens=True,
+        max_length = self.max_len,
+        return_token_type_ids = False,
+        padding = 'max_length',
+        return_attention_mask= True,
+        return_tensors='pt',
+        truncation=True
+    )
+
+    return {
+        'text' : text,
+        'input_ids' : encoding['input_ids'].flatten(),
+        'attention_mask' : encoding['attention_mask'].flatten(),
+        'label' : torch.tensor(label,dtype=torch.long)
+
+    }
 import seaborn as sns
 def show_confusion_matrix(confusion_matrix):
   hmap = sns.heatmap(confusion_matrix, annot=True, fmt="d", cmap="Blues")
@@ -49,5 +84,4 @@ def show_confusion_matrix(confusion_matrix):
   hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation=30, ha='right')
   plt.ylabel('True sentiment')
   plt.xlabel('Predicted sentiment')
- 
   
